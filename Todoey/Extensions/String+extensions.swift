b3f5e7c714eb4dd61549e847872236b1fbc20e2d
Justin Honda <justinohonda@gmail.com>
2020-08-02 14:45:23 -0400

    Scaffolded project

---
+++ Todoey/Extensions/String+extensions.swift
@@ -0,0 +1,13 @@
+//
+//  String+extensions.swift
+//  Todoey
+//
+//  Created by Justin Honda on 8/2/20.
+//  Copyright © 2020 Laguna Labs. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    static let Empty = ""
+}
---
+++ Todoey/Models/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Justin Honda on 8/2/20.
+//  Copyright © 2020 Laguna Labs. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = String.Empty
+    var isDone: Bool = false
+}
